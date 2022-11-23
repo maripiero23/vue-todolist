ef8c5d9be7400673826f0287bc0ba1e142469332
maripiero23 <mari.pieropan@gmail.com>
2022-11-23 16:31:10 +0100

    js start

--- js/main.js
+++ js/main.js
@@ -3,12 +3,27 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
+            toDoList: [
+                {
+                    text: "testo del task1",
+                    done: false
+                },
 
+                {
+                    text: "testo del task2",
+                    done: false
+                },
+
+                {
+                    text: "testo del task3",
+                    done: false
+                }
+            ]
         };
     },
 
     methods: {
-        
+
     }
 
 }).mount("#app")
